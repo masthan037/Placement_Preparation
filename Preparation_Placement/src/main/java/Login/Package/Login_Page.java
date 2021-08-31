6c9a6df5461f6df3b8382b8e8ecfb48191cc8ab6
masthan037 <masthanirfan037@gmail.com>
2021-08-31 18:08:58 +0530

    Attached HTML CSS Files To JSP

---
+++ Preparation_Placement/src/main/java/Login/Package/Login_Page.java
@@ -0,0 +1,40 @@
+package Login.Package;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login_Page
+ */
+@WebServlet("/Login_Page")
+public class Login_Page extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login_Page() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("<h1>Hello People</h1>");
+		
+	}
+
+	/**	
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
--- Preparation_Placement/src/main/webapp/Preparation.jsp
+++ Preparation_Placement/src/main/webapp/Preparation.jsp
@@ -1,3 +1,4 @@
+<%--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 --%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -7,7 +8,12 @@
 <title>Insert title here</title>
 </head>
 <body>
-<%--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 --%>
-
+<H1>This is my first JSP</H1>
+<h2><%
+int number = 63;
+out.print("<br/>");
+if(number == 53) out.println("Masthan number");
+else out.println("It's not masthan's number");
+%></h2>
 </body>
 </html>
